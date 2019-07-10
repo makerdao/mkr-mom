8e0f151b323cb6fe9c4b4df800358aa2a18c182f
Kurt Barry <kurt.m.barry@gmail.com>
2019-07-09 20:33:15 -0700

    setOwner tests

--- src/MkrMom.t.sol
+++ src/MkrMom.t.sol
@@ -4,17 +4,43 @@ import "ds-test/test.sol";
 
 import "./MkrMom.sol";
 
+contract MkrMomCaller {
+  MkrMom mom;
+
+  constructor(MkrMom mom_) public {
+    mom = mom_;
+  }
+
+  function setOwner(address newOwner) public {
+    mom.setOwner(newOwner);
+  }
+}
+
 contract MkrMomTest is DSTest {
-    DSToken fkr;
-    MkrMom mom;
+  DSToken fkr;
+  MkrMom mom;
+  MkrMomCaller caller;
 
-    function setUp() public {
-        fkr = new DSToken("FKR");
-        mom = new MkrMom(fkr);
-    }
+  function setUp() public {
+    fkr = new DSToken("FKR");
+    mom = new MkrMom(fkr);
+    caller = new MkrMomCaller(mom);
+  }
 
-    function verifyConstruction() public {
-      assertTrue(mom.owner() == address(this));
-      assertTrue(mom.mkr() == fkr);
-    }
+  function testVerifyConstruction() public {
+    assertTrue(mom.owner() == address(this));
+    assertTrue(mom.mkr() == fkr);
+  }
+
+  function testSetOwner() public {
+    mom.setOwner(address(caller));      
+    assertTrue(mom.owner() == address(caller));
+    caller.setOwner(address(this));
+    assertTrue(mom.owner() == address(this));
+  }
+
+  function testFailSetOwner() public {
+    assertTrue(mom.owner() != address(caller));
+    caller.setOwner(address(caller));
+  }
 }
